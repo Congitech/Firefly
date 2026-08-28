a1a637d01d9207f4138574084b1e03ed39a06759
Congitech <qiuqiucong666@gmail.com>
2026-08-28 13:44:50 +0800

    init project

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -127,7 +127,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-08-28",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
@@ -240,7 +240,7 @@ export const siteConfig: SiteConfig = {
 	// ── Bilibili配置 ──────────────────────────────────
 	bilibili: {
 		// 你的 Bilibili 用户 UID
-		uid: "38932988",
+		uid: "2036305433",
 	},
 
 	// ── 番组计划bangumi配置 ──────────────────────────────────
