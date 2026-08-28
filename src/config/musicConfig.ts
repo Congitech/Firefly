73e1a5d8cac02997b332fcb5c632178104912447
Congitech <qiuqiucong666@gmail.com>
2026-08-28 13:04:44 +0800

    这是我的博客

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -48,9 +48,9 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	local: {
 		playlist: [
 			{
-				name: "使一颗心免于哀伤",
-				artist: "知更鸟 / HOYO-MiX / Chevy",
-				url: "/assets/music/使一颗心免于哀伤-哼唱.mp3",
+				name: "最佳损友",
+				artist: "陈奕迅 / blues / Eason",
+				url: "/assets/music/最佳损友 - 陈奕迅.mp3",
 				cover: "/assets/music/cover/109951169585655912.webp",
 				lrc: "",
 			},
--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -40,21 +40,21 @@ const pages = resolvePageToggles({
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "Quinn",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "personal blog",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://quinn.qd.je",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"Welcome to Quinn's blog",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
+		"Quinn",
 		"Fuwari",
 		"Astro",
 		"ACGN",
@@ -115,7 +115,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "Quinn",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
